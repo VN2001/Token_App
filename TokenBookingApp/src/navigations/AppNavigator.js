1c2bb8a15905734ae0cf7ccda2b3ae20933b121a
Nirmal-011 <nirmalsep2001@gmail.com>
2026-02-15 00:14:37 +0530

    Admin folder and login

--- TokenBookingApp/src/navigations/AppNavigator.js
+++ TokenBookingApp/src/navigations/AppNavigator.js
@@ -3,8 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import SplashScreen from "../screens/SplashScreen";
-import LoginScreen from "../screens/LoginScreen";
-
+import AdminLoginScreen from "../screens/Admin/AdminLoginScreen"
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
@@ -12,7 +11,7 @@ const AppNavigator = () => {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Splash" component={SplashScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Login" component={AdminLoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
